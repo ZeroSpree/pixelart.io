8ae9100b61ba71a49c2521cfa9b51fecf0655a32
Codrin Pavel <zerospree@yahoo.com>
2018-12-04 16:37:43 +0200

    JekyllCMS Publish

--- gulpfile.git.js
+++ gulpfile.git.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
-var git = require('gulp-git');
+var gulp        = require('gulp');
+var git         = require('gulp-git');
 var runSequence = require('run-sequence');
+var notify      = require('gulp-notify');
 
 module.exports = function() {
 
@@ -15,10 +16,13 @@ module.exports = function() {
         return gulp
             .src('.')
             .pipe( git.add() )
+            .pipe(notify({ message: 'Ran git.add()' }))
             .pipe( git.commit('JekyllCMS Publish') )
+            .pipe(notify({ message: 'Ran git.commit()' }))
             .pipe( git.push('origin', 'master', function(err) {
                 if (err) throw err;
-            }) );
+            }) )
+            .pipe(notify({ message: 'Ran git.push()' }));
     });
 
     gulp.task('git:publish', function() {
