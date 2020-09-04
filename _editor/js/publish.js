abb6da29f978d9c9b70e0911c39db63795a75ad9
Codrin Pavel <zerospree@yahoo.com>
2020-09-04 22:49:01 +0300

    Automatic Deploy

--- _editor/js/publish.js
+++ _editor/js/publish.js
@@ -1,6 +1,7 @@
 function publish () {
     var data = {
-        cmd: 'gulp git:publish'
+        //cmd: 'gulp git:publish'
+        cmd: 'git grep attracting-talent.jpg'
     }
 
     $.post(config.routes.exec, data, function (res) {
